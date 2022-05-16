13b7c5db4cf60331f548599da4a0060d6ccc78f3
Alexcolman Tochukwu Apunike <alexcolman.apunike@gmail.com>
2022-05-15 22:46:17 -0300

    Criação dos componentes Header, Footer, Nav, Filters & Product List.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.component';
+import { ProductListComponent } from './components/bookstore-app/product-list/product-list.component';
+import { FiltersComponent } from './components/bookstore-app/filters/filters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    BookstoreAppComponent,
+    ProductListComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
